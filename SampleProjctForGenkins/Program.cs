b27792c53336b3111cf1596be54a8f5099898ce6
Somnathgarule22 <somnathgarule22@gmail.com>
2025-07-25 09:23:35 +0530

    Add project files.

---
+++ SampleProjctForGenkins/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProjctForGenkins
+{
+    internal class Program
+    {
+        public void Addtion()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine("Addition is: " + c);
+        }
+        static void Main(string[] args)
+
+        {
+            Program program = new Program();
+            program.Addtion();
+            Console.ReadLine(); 
+        }
+    }
+}
